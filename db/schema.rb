bbbd290ebd20171134b505ced10068b2637456c8
CallumDougan <practicalitymalfunction@gmail.com>
2016-01-05 11:49:43 +0000

    added Copy jointable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160105114040) do
+ActiveRecord::Schema.define(version: 20160105114902) do
+
+  create_table "copies", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.string   "name"
